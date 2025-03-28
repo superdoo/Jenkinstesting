83d1fe288866da9e72287e3dcccb74691f8bd5c0
mbarreras <wbbmb2016@gmail.com>
2025-03-28 14:01:29 -0500

    round two testing

--- job.py
+++ job.py
@@ -1,1 +1,8 @@
-print("hello from me")
+import datetime
+
+def main():
+    now = datetime.datetime.now()
+    print(f"Michael's Jenkins Job Executed Successfully at {now}")
+
+if __name__ == "__main__":
+    main()
